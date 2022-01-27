828ba42368cf0a57776d9d03a8b08e01b5f3f07f
Panyapat Wongdee <panyapat12345@gmail.com>
2022-01-28 00:28:28 +0700

    modified all functions , added checkbox function

--- Game.cpp
+++ Game.cpp
@@ -1,20 +1,26 @@
 #include<iostream>
 using namespace std;
 
+void setMatrix(int ** p, int n);            // test function
+void EXsuduku(int **p, int n);              // example suduku
+void set0(int ** p, int n);
+
 void newMatrix(int ** &p, int n = 3);
 void deleteMatrix(int **p, int n = 3);
-void setMatrix(int ** p, int n);            // test function
 void printTable(int **p, int n);
-bool UsedInRow(int **p, int N,int number); //N = 9
-bool UsedInCol(int **p, int N,int number);
+bool checkRow(int **p, int N,int number); //N = 9
+bool checkCol(int **p, int N,int number);
+bool checkBox(int ** p, int n, int x, int y, int num);
+
 
 int main(){
 
     int **p;
-    newMatrix(p);       //Array 9*9
-    setMatrix(p, 9);
-    printTable(p, 3);
+    newMatrix(p);       // box 3*3 --> Array 9*9
 
+    
+
+    printTable(p, 3);
     deleteMatrix(p);
 
     return 0;
@@ -30,16 +36,6 @@ void deleteMatrix(int **p, int n){
     delete [] p;
 }
 
-// test function
-void setMatrix(int ** p, int n){
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++)
-            p[i][j] = j;
-    }
-
-}
-
 void printTable(int **p, int n){
     cout << "-------------------------\n";
     for(int i = 0; i < n*n; i++){
@@ -53,7 +49,7 @@ void printTable(int **p, int n){
     cout << "-------------------------";
 }
 
-bool UsedInRow(int **p,int N, int number)
+bool checkRow(int **p,int N, int number)
 {   
     for (int i = 1; i < N+1; i++)
     {
@@ -63,7 +59,7 @@ bool UsedInRow(int **p,int N, int number)
         return false;
     }
 }
-bool UsedInCol(int **p,int N ,int number)
+bool checkCol(int **p,int N ,int number)
 {
     for (int row = 0; row < N; row++)
     {
@@ -73,4 +69,45 @@ bool UsedInCol(int **p,int N ,int number)
         else 
         return false;
     }
+}
+
+bool checkBox(int ** p, int n, int x, int y, int num){
+    x = (x/3)*3;
+    y = (y/3)*3;
+    
+    for(int i = y; i < y+3; i++){
+        for(int j = x; j < x+3; j++){
+            if(p[i][j] == num)      return true;
+        }
+    }
+    return false;
+}
+
+// example suduku
+void EXsuduku(int **p, int n){
+
+    int a[9*9] =    { 2,8,3,7,9,5,4,1,6,
+                      6,9,1,8,4,2,5,3,7,
+                      4,7,5,6,3,1,2,9,8,
+                      7,5,6,9,8,4,3,2,1,
+                      1,3,9,5,2,6,7,8,4,
+                      8,2,4,1,7,3,6,5,9,
+                      9,4,2,3,6,8,1,7,5,
+                      5,6,7,2,1,9,8,4,3,
+                      3,1,8,4,5,7,9,6,2 };
+
+    for(int i = 0; i < n*n; i++){
+        for(int j = 0; j < n*n; j++)
+            p[i][j] = a[i*n*n+j];
+    }
+}
+
+// set value to zero
+void set0(int ** p, int n){
+    
+    for(int i = 0; i < n*n; i++){
+        for(int j = 0; j < n*n; j++)
+            p[i][j] = 0;
+    }
+   
 }
